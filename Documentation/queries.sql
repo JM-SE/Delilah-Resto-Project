4649896ab797fb1be9a04db192908800da11d8ae
Jose Maria Silva Ezpeleta <jsilva11@gmail.com>
2020-05-07 19:32:51 -0300

    Queries error correction

--- Documentation/queries.sql
+++ Documentation/queries.sql
@@ -1,24 +1,16 @@
--- Drop Tables
-DROP TABLE users;
-DROP TABLE products;
-DROP TABLE carts;
-DROP TABLE cartItems;
-DROP TABLE orders;
-DROP TABLE orderItems;
 
 -- Populate users table
 INSERT INTO
   users
 VALUES
   (
-    NULL,
     "admin",
     "admin",
     "admin",
     "admin@delilah.com",
     000,
     "Delilah Street 99",
-    1,
+    1
   );
 
 
@@ -27,82 +19,75 @@ INSERT INTO
   products
 VALUES
   (
-    NULL,
     "Veggie Sandwich",
     280,
     "VS",
     "sandveggie.jpg",
-    "sandveggie.jpg",
+    "sandveggie.jpg"
   );
 
 INSERT INTO
   products
 VALUES
   (
-    NULL,
     "Cheese Omelette",
     260,
     "CO",
     "omelettequeso.jpg",
-    "omelettequeso.jpg",
+    "omelettequeso.jpg"
   );
 
 INSERT INTO
   products
 VALUES
   (
-    NULL,
     "Veggie Rolls",
     270,
     "VR",
     "rollsverdu.jpg",
-    "rollsverdu.jpg",
+    "rollsverdu.jpg"
   );
 
 INSERT INTO
   products
 VALUES
   (
-    NULL,
     "Lemon Cookies",
     110,
     "LC",
     "lemoncookies.jpg",
-    "lemoncookies.jpg",
+    "lemoncookies.jpg"
   );
 
 INSERT INTO
   products
 VALUES
   (
-    NULL,
     "Fruits Yoghurt",
     150,
     "YG",
     "yoghurt.jpg",
-    "yoghurt.jpg",
+    "yoghurt.jpg"
   );
 
 INSERT INTO
   products
 VALUES
   (
-    NULL,
     "Strawberries",
     80,
     "ST",
     "strawberries.jpg",
-    "strawberries.jpg",
+    "strawberries.jpg"
   );
 
 INSERT INTO
   products
 VALUES
   (
-    NULL,
     "Coffee",
     60,
     "CF",
     "coffee.jpg",
-    "coffee.jpg",
+    "coffee.jpg"
   );
